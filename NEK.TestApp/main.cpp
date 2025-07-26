790ed762b41cc632c3b1233d93e4bcf5e0210e1b
Ekrynox <lucas123.a17@gmail.com>
2025-07-26 14:35:42 +0200

    First Version of Nikon Camera filtering

--- NEK.TestApp/main.cpp
+++ NEK.TestApp/main.cpp
@@ -3,7 +3,8 @@
 
 
 int main() {
-	std::cout << "Nikon devices found: " << countUsbDevices() << std::endl;
+	std::cout << "USB devices found: " << countUsbDevices() << std::endl;
+	std::cout << "Nikon cameras found: " << countNikonCameras() << std::endl;
 
 	return 0;
 }
