42b0681d1ff5953301708dd7280c5787c802a4ed
Ekrynox <lucas123.a17@gmail.com>
2025-07-26 14:35:42 +0200

    First Version of Nikon Camera filtering

--- NEK/nek_usb.cpp
+++ NEK/nek_usb.cpp
@@ -31,5 +31,42 @@ namespace nek {
 
 			return count;
 		}
+
+		ssize_t UsbManager::countNikonCameras() {
+			libusb_device** devices;
+
+			ssize_t count = libusb_get_device_list(context_, &devices);
+			if (count < 0) {
+				throw std::system_error(static_cast<int>(count), std::generic_category(), "Failed to get libusb device list");
+			}
+
+			ssize_t countNikon = 0;
+			for (ssize_t i = 0; i < count; ++i) {
+				if (isNikonCamera(devices[i])) {
+					++countNikon;
+				}
+			}
+
+			libusb_free_device_list(devices, 1);
+
+			return countNikon;
+		}
+
+
+		bool UsbManager::isNikonCamera(libusb_device* device) {
+			libusb_device_descriptor desc;
+
+			int res = libusb_get_device_descriptor(device, &desc);
+			if (res != 0) {
+				throw std::system_error(static_cast<int>(res), std::generic_category(), "Failed to get libusb device descriptor");
+			}
+
+			// Nikon vendor ID
+			if (desc.idVendor != 0x04b0) {
+				return false;
+			}
+
+			return true;
+		}
 	}
 }
