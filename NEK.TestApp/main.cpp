be22a9d48e816a7de260c0936384486bc2a04925
Ekrynox <lucas123.a17@gmail.com>
2025-07-28 22:04:24 +0200

    New MTP Maneger implementation with WPD encapsulated in Singleton.

--- NEK.TestApp/main.cpp
+++ NEK.TestApp/main.cpp
@@ -3,8 +3,8 @@
 
 
 int main() {
-	std::cout << "USB devices found: " << countUsbDevices() << std::endl;
 	std::cout << "Nikon cameras found: " << countNikonCameras() << std::endl;
 
+	std::cout << test() << std::endl;
 	return 0;
 }
