e861671654b851ea3c8e927fa5339dc323ba7c26
Ekrynox <lucas123.a17@gmail.com>
2025-07-29 20:45:50 +0200

    Event Management and Callback v1

--- NEK/nek_mtp_utils.cpp
+++ NEK/nek_mtp_utils.cpp
@@ -1,6 +1,7 @@
 #include "nek_mtp_utils.hpp"
 
 #include <stdexcept>
+#include <iostream>
 
 #include <propvarutil.h>
 
@@ -9,6 +10,7 @@
 using namespace nek::mtp;
 
 
+//MtpParams
 MtpParams::MtpParams() {
 	HRESULT hr = CoCreateInstance(CLSID_PortableDevicePropVariantCollection, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&paramsCollection_));
 	if (FAILED(hr)) {
@@ -47,4 +49,51 @@ void MtpParams::addInt16(int16_t value) {
 	InitPropVariantFromInt16(value, &pv);
 	paramsCollection_->Add(&pv);
 	PropVariantClear(&pv);
+}
+
+
+
+//MtpEventCallback
+MtpEventCallback::MtpEventCallback() : ref_(0), nextId(0) {}
+
+HRESULT MtpEventCallback::OnEvent(IPortableDeviceValues* pEventParameters) {
+	std::lock_guard<std::mutex> lock(mutex_);
+
+	for (auto& [id, callback] : callbacks_) {
+		callback(pEventParameters);
+	}
+	return HRESULT(0);
+}
+
+HRESULT MtpEventCallback::QueryInterface(REFIID riid, void** ppv) {
+	static const QITAB qitab[] = { QITABENT(MtpEventCallback, IPortableDeviceEventCallback), { }, };
+	return QISearch(this, qitab, riid, ppv);
+}
+
+ULONG MtpEventCallback::AddRef() {
+	return InterlockedIncrement(&ref_);
+}
+
+ULONG MtpEventCallback::Release() {
+	ULONG ref = _InterlockedDecrement(&ref_);
+	if (ref == 0) {
+		delete this;
+	}
+
+	return ref;
+}
+
+
+size_t MtpEventCallback::RegisterCallback(std::function<void(IPortableDeviceValues*)> callback) {
+	std::lock_guard<std::mutex> lock(mutex_);
+
+	size_t id = nextId++;
+	callbacks_.emplace_back(id, callback);
+	return id;
+}
+
+void MtpEventCallback::UnregisterCallback(size_t id) {
+	std::lock_guard<std::mutex> lock(mutex_);
+
+	callbacks_.erase(std::remove_if(callbacks_.begin(), callbacks_.end(), [id](const auto& pair) { return pair.first == id; }), callbacks_.end());
 }
