a642b91a442e9e89f80fa59319b0c15a6675b7f8
Ekrynox <lucas123.a17@gmail.com>
2025-07-30 21:32:29 +0200

    Working GetDeviceInfo, implement enum/struct for Operation/Response/Code, and add exception class

---
+++ NEK/nek_mtp_except.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "nek.hpp"
+
+#include <stdlib.h>
+#include <exception>
+#include <string>
+
+
+
+namespace nek {
+	namespace mtp {
+
+		class NEK_API MtpException : public std::exception {
+		public:
+			MtpException(uint32_t operationCode, uint32_t responseCode);
+
+			uint32_t operationCode() const;
+			uint32_t responseCode() const;
+
+		private:
+			uint32_t operationCode_;
+			uint32_t responseCode_;
+		};
+
+	}
+}
