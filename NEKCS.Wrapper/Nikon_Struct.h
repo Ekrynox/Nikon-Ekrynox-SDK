121c534cc24b96125e2f489aa18b4ba0d7b14e7a
Ekrynox <lucas123.a17@gmail.com>
2025-07-31 17:27:34 +0200

    First Version of C# Wrapper and Test App with device List. Fix error with COM and C++/CLI & fix refresh device list.

---
+++ NEKCS.Wrapper/Nikon_Struct.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "nikon_struct.hpp"
+
+
+
+namespace NEKCS {
+
+	public ref struct NikonDeviceInfoDS {
+		System::UInt16 StandardVersion = 0;
+		System::UInt32 VendorExtensionID = 0;
+		System::UInt16 VendorExtensionVersion = 0;
+		System::String^ VendorExtensionDesc = "";
+		System::UInt16 FunctionalMode = 0;
+		System::Collections::Generic::List<System::UInt16> OperationsSupported;
+		System::Collections::Generic::List<System::UInt16> EventsSupported;
+		System::Collections::Generic::List<System::UInt16> DevicePropertiesSupported;
+		System::Collections::Generic::List<System::UInt16> CaptureFormats;
+		System::Collections::Generic::List<System::UInt16> ImageFormats;
+		System::String^ Manufacture = "";
+		System::String^ Model = "";
+		System::String^ DeviceVersion = "";
+		System::String^ SerialNumber = "";
+
+		NikonDeviceInfoDS();
+		NikonDeviceInfoDS(const nek::NikonDeviceInfoDS& native);
+	};
+
+}
