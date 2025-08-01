f3177e218c807f13bbed2bac941d452f06d628f2
Ekrynox <lucas123.a17@gmail.com>
2025-08-01 21:36:19 +0200

    Threaded MtpManager

--- NEK/mtp/nek_mtp.hpp
+++ NEK/mtp/nek_mtp.hpp
@@ -5,8 +5,13 @@
 #include "nek_mtp_struct.hpp"
 #include "nek_mtp_except.hpp"
 
+#include <atomic>
+#include <future>
 #include <map>
+#include <mutex>
+#include <queue>
 #include <string>
+#include <thread>
 #include <vector>
 
 #include <atlbase.h>
@@ -32,17 +37,28 @@ namespace nek {
 
 			std::map<std::wstring, MtpDeviceInfoDS> listMtpDevices();
 			size_t countMtpDevices();
-			CComPtr<IPortableDevice> openDevice(const PWSTR devicePath);
 
 		private:
-			CComPtr<IPortableDeviceManager> deviceManager_;
-			CComPtr<IPortableDeviceValues> deviceClient_;
-
 			MtpManager& operator= (const MtpManager&) = delete;
 			MtpManager(const MtpManager&) = delete;
 			MtpManager();
 			~MtpManager();
+			void threadTask();
+
+			void sendTaskAsync(std::function<void()> task);
+			void sendTask(std::function<void()> task);
+			template<typename T> T sendTaskWithResult(std::function<T()> task);
+
+			CComPtr<IPortableDeviceManager> deviceManager_;
+			std::queue<std::function<void()>> tasks_;
+			std::thread thread_;
+			std::atomic<bool> running_;
+			std::mutex mutexDevice_;
+			std::mutex mutexTasks_;
+			std::condition_variable cvTasks_;
 		};
+
+
 
 
 		class NEK_API MtpDevice {
@@ -62,6 +78,7 @@ namespace nek {
 
 
 		private:
+			CComPtr<IPortableDeviceValues> deviceClient_;
 			CComPtr<IPortableDevice> device_;
 			CComPtr<MtpEventCallback> eventCallback_;
 			PWSTR eventCookie;
