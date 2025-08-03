14976297be84d527146ce9001de282315973648a
Ekrynox <lucas123.a17@gmail.com>
2025-08-03 03:01:45 +0200

    Implement Get_Event & Get_EventEx

--- NEK.TestApp/main.cpp
+++ NEK.TestApp/main.cpp
@@ -7,8 +7,10 @@ using namespace std;
 
 
 void eventFunc (nek::mtp::MtpEvent event) {
-	int a;
-	cout << "event" << std::endl;
+	cout << "event: " << std::hex << event.eventCode << std::endl;
+	for (uint32_t p : event.params) {
+		cout << "\t: " << std::hex << p << std::endl;
+	}
 	return;
 }
 
@@ -23,16 +25,16 @@ int main() {
 		wcout << cameraInfo.second.Manufacture << " " << cameraInfo.second.Model << " " << cameraInfo.second.SerialNumber << endl;
 	}
 
-	auto camera = nek::mtp::MtpDevice((PWSTR)nikonCameras.begin()->first.c_str());
+	auto camera = nek::NikonCamera(nikonCameras.begin()->first);
 	camera.RegisterCallback(eventFunc);
 
 
 	int wait = 1;
 	while (wait > 0) {
+		cin >> wait;
 		auto params = nek::mtp::MtpParams();
 		params.addUint32(0xFFFFFFFF);
 		nek::mtp::MtpResponse result = camera.SendCommand(nek::NikonMtpOperationCode::InitiateCaptureRecInSdram, params);
-		cin >> wait;
 	} 
 
 	return 0;
