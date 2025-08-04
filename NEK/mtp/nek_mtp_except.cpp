36e2e25b7d1d21fdbb4a80c993e830b1bddbeecb
Ekrynox <lucas123.a17@gmail.com>
2025-08-04 20:20:53 +0200

    Start new Exception Management System

--- NEK/mtp/nek_mtp_except.cpp
+++ NEK/mtp/nek_mtp_except.cpp
@@ -1,14 +1,56 @@
 #include "nek_mtp_except.hpp"
 
+#include <sstream>
+
 
 
 using namespace nek::mtp;
 
 
-MtpException::MtpException(uint32_t operationCode, uint32_t responseCode) {
-	operationCode_ = operationCode;
-	responseCode_ = responseCode;
+
+//MtpDeviceException
+MtpDeviceException::MtpDeviceException(MtpExPhase phase, HRESULT hr) : phase(phase), code(computeCode(phase, hr)) {};
+
+MtpExCode MtpDeviceException::computeCode(MtpExPhase phase, HRESULT hr) {
+	if (hr == S_OK) return NO_ERR;
+	if (hr == E_OUTOFMEMORY) return MEMORY;
+	if (hr == E_UNEXPECTED) return UNEXPECTED;
+	if (hr == E_INVALIDARG) return INVALIDARG;
+
+	switch (phase) {
+	case COM_INIT:
+		switch (hr) {
+		case S_FALSE:
+		case RPC_E_CHANGED_MODE:
+			return ALREADY_INITIALIZED;
+		default:
+			return UNKNOW_ERR;
+		}
+
+	case MANAGER_INIT:
+		switch (hr) {
+		case E_POINTER:
+		case E_NOINTERFACE:
+		case CLASS_E_NOAGGREGATION:
+		case REGDB_E_CLASSNOTREG:
+			return INVALIDARG;
+		default:
+			return UNKNOW_ERR;
+		}
+
+	default:
+		return UNKNOW_ERR;
+	}
 }
 
-uint32_t MtpException::operationCode() const { return operationCode_; }
-uint32_t MtpException::responseCode() const { return responseCode_; }
+
+
+//MtpException
+MtpException::MtpException(uint32_t operationCode, uint32_t responseCode) : operationCode(operationCode), responseCode(responseCode) {}
+
+const char* MtpException::what() const noexcept {
+	std::ostringstream message;
+	message << "MTP Error - Operation: 0x" << std::hex << operationCode;
+	message << ", Response: 0x" << std::hex << responseCode;
+	return message.str().c_str();
+}
