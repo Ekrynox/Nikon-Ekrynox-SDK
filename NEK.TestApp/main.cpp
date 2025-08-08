8649f4a75090fb97ab7892ef52fe63bb20f4877c
Ekrynox <lucas123.a17@gmail.com>
2025-08-08 11:36:44 +0200

    Improve Read/GetDeviceInfo data copy

--- NEK.TestApp/main.cpp
+++ NEK.TestApp/main.cpp
@@ -32,6 +32,17 @@ void eventFunc (nek::mtp::MtpEvent event) {
 			MyFile.write((char*)result.data.data(), result.data.size());
 			MyFile.close();
 		}
+
+		{
+			auto params = nek::mtp::MtpParams();
+			params.addUint32(nek::NikonMtpDevicePropCode::Fnumber);
+			vector<BYTE> data;
+			data.resize(sizeof(uint16_t));
+			uint16_t fnum = 450;
+			memcpy(data.data(), &fnum, sizeof(uint16_t));
+			nek::mtp::MtpResponse result = camera->SendCommandAndWrite(nek::NikonMtpOperationCode::SetDevicePropValue, params, data);
+			cout << std::hex << "Response code: " << result.responseCode << endl;
+		}
 	}
 
 	return;
@@ -63,10 +74,23 @@ int main() {
 		cin >> wait;
 		if (wait == 0) break;
 
-		auto params = nek::mtp::MtpParams();
-		params.addUint32(0xFFFFFFFF);
-		nek::mtp::MtpResponse result = camera->SendCommand(nek::NikonMtpOperationCode::InitiateCaptureRecInSdram, params);
-		cout << std::hex << "Response code: " << result.responseCode << endl;
+		{
+			auto params = nek::mtp::MtpParams();
+			params.addUint32(nek::NikonMtpDevicePropCode::Fnumber);
+			vector<BYTE> data;
+			data.resize(sizeof(uint16_t));
+			uint16_t fnum = 1100;
+			memcpy(data.data(), &fnum, sizeof(uint16_t));
+			nek::mtp::MtpResponse result = camera->SendCommandAndWrite(nek::NikonMtpOperationCode::SetDevicePropValue, params, data);
+			cout << std::hex << "Response code: " << result.responseCode << endl;
+		}
+
+		{
+			auto params = nek::mtp::MtpParams();
+			params.addUint32(0xFFFFFFFF);
+			nek::mtp::MtpResponse result = camera->SendCommand(nek::NikonMtpOperationCode::InitiateCaptureRecInSdram, params);
+			cout << std::hex << "Response code: " << result.responseCode << endl;
+		}
 	}
 
 	delete camera;
