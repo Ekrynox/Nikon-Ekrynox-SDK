f016890d2c31831342845148ddc6200421a4900e
Ekrynox <lucas123.a17@gmail.com>
2025-08-21 03:42:55 +0200

    Try Get Integer/Uinteger/arrayint/Arrayuint in the wrapper

--- NEKCS.Wrapper/Nikon_Struct.h
+++ NEKCS.Wrapper/Nikon_Struct.h
@@ -85,6 +85,11 @@ namespace NEKCS {
 
 		NikonMtpDatatypeCode GetType() { return m_type; };
 
+		System::Boolean TryGetInteger([Out] System::Int64% data);
+		System::Boolean TryGetUInteger([Out] System::UInt64% data);
+		System::Boolean TryGetArrayInteger([Out] array<System::Int64>^% data);
+		System::Boolean TryGetArrayUInteger([Out] array<System::UInt64>^% data);
+
 		System::Boolean TryGetInt8([Out] System::SByte% data) { return TryGet(data); };
 		System::Boolean TryGetUInt8([Out] System::Byte% data) { return TryGet(data); };
 		System::Boolean TryGetInt16([Out] System::Int16% data) { return TryGet(data); };
@@ -145,6 +150,11 @@ namespace NEKCS {
 		NikonDevicePropDescDS_Variant(const nek::mtp::MtpDevicePropDescDSV& desc);
 
 		template<typename T> System::Boolean TryGet([Out] NikonDevicePropDescDS<T>^% desc);
+
+		System::Boolean TryGetInteger([Out] NikonDevicePropDescDS<System::Int64>^% desc);
+		System::Boolean TryGetUInteger([Out] NikonDevicePropDescDS<System::UInt64>^% desc);
+		System::Boolean TryGetArrayInteger([Out] NikonDevicePropDescDS<array<System::Int64>^>^% desc);
+		System::Boolean TryGetArrayUInteger([Out] NikonDevicePropDescDS<array<System::UInt64>^>^% desc);
 
 		System::Boolean TryGetInt8([Out] NikonDevicePropDescDS<System::SByte>^% desc) { return TryGet(desc); };
 		System::Boolean TryGetUInt8([Out] NikonDevicePropDescDS<System::Byte>^% desc) { return TryGet(desc); };
